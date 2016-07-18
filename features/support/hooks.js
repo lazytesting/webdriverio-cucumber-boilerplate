cc50d6646b87697fad66e39afb1b50dd5bb2689b
Maarten Groeneweg <mgroeneweg@mirabeau.nl>
2016-07-18 15:24:17 +0200

    take screenshot on failed test

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,10 +1,28 @@
+const config = require('../../config.json');
+
 var myHooks = function () {
+
     this.Before(function (scenario, callback) {
-        this.browser.init().then(function(){callback()});
+        this.browser.init()
+            .then(function(){callback()});
     });
+
     this.After(function (scenario, callback) {
-        this.browser.end().then(function(){callback()});
+        this.browser.end()
+            .then(()=>callback());
     });
+
+    this.After(function (scenario, callback) {
+        if (scenario.isFailed()) {
+            this.browser.saveScreenshot(config.screenshotFolder + scenario.getName() + ".png").then(function () {
+                callback()
+            });
+        }
+        else {
+            callback();
+        }
+    });
+
 };
 
 module.exports = myHooks;
